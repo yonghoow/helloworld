b6ae1d30f8a9a861e039a25a43cfff9219e6f3c8
yonghoow <2073951b@student.tp.edu.sg>
2024-06-22 15:34:11 +0800

    initial commit on 22 Jun 24

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+
+namespace helloworld
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string pathtoFile = @"testnote.txt";
+
+            Process.Start(@"notepad.exe", pathtoFile);
+
+            
+        }
+    }
+}
+
